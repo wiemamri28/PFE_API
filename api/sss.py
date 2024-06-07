93993d45f2b5bd54ed72931d58abec1f88b02e02
wiemarmi28 <wiemarmi28@gmail.com>
2024-06-08 00:05:22 +0100

    init commit

--- api/sss.py
+++ api/sss.py
@@ -9,3 +9,9 @@ def save_module(url, save_path):
 
 
 save_module("https://kaggle.com/models/rishitdagli/plant-disease/frameworks/TensorFlow2/variations/plant-disease/versions/1", "./saved-module")
+
+# https://www.kaggle.com/models/rishitdagli/plant-disease/TensorFlow2/plant-disease/1/code
+
+# https://www.kaggle.com/code/vikash013/project
+
+# https://www.kaggle.com/models/rishitdagli/plant-disease/TensorFlow2/plant-disease/1#:~:text=New%20Notebook-,About%20Variation,-FINE%2DTUNABLE
